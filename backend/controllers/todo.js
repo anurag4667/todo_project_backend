2d938aa81a1abbfacbdede2a55d42f92a4281394
Anurag Rajput <arajput3000@gmail.com>
2024-01-04 00:12:45 +0530

    added edit todo

--- backend/controllers/todo.js
+++ backend/controllers/todo.js
@@ -27,4 +27,42 @@ const createtodo = async(req,res)=>{
 
 }
 
-export {createtodo};
+const edittodo = async (req,res) =>{
+    try {
+
+        const {title,description} = req.body;
+
+        const todo = await Todo.findById(req.params.id);
+        const user = await User.findById(req.user._id);
+
+        if(!todo){
+            return res.status(404).json({
+                success : false,
+                message : "todo not found"
+            })
+        }
+
+        if(!user.todos.includes(todo._id)){
+            return res.status(401).json({
+                success : false,
+                message : "unauthorized"
+            })
+        }
+
+        if(title)todo.title = title;
+        if(description)todo.description = description;
+        await todo.save();
+
+        res.status(200).json({
+            success : true,
+            message : "todo edited",
+        })
+        
+    } catch (error) {
+        res.status(500).json({
+            success : false,
+            message : error.message
+        })
+    }
+}
+export {createtodo , edittodo};
--- backend/routes/todo.js
+++ backend/routes/todo.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createtodo } from "../controllers/todo.js";
+import { createtodo, edittodo } from "../controllers/todo.js";
 import authentication from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.route("/addtodo").post(authentication,createtodo);
+router.route("/edittodo/:id").put(authentication,edittodo);
 
 export default router;
